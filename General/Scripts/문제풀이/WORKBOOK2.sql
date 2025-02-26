570c62c4fb008861da0d067f9998b5a728825573
holelung <junho335276@gmail.com>
2025-02-26 17:08:20 +0900

    20250226_DDL

--- General/Scripts/문제풀이/WORKBOOK2.sql
+++ General/Scripts/문제풀이/WORKBOOK2.sql
@@ -1,7 +1,9 @@
 -- WORKBOOK2 FUNCTION
 
 /** 1번 **/
-
+-- 영어영문학과(학과코드 002) 학생들의 학번과 이름, 입학 년도를
+-- 입학 년도가 빠른 순으로 표시하는 SQL을 작성하시오.
+-- (단, 헤더는 "학번", "이름", "입학년도" 가 표시되도록 한다.)
 SELECT 
 	STUDENT_NO AS 학번,
 	STUDENT_NAME AS 이름,
@@ -11,15 +13,23 @@ WHERE DEPARTMENT_NO = '002'
 ORDER BY ENTRANCE_DATE ASC;
 
 
+
+-------------------------------------------------------------------
 /** 2번 **/
+-- 춘 기술대학교의 교수 중 이름이 세 글자가 아닌 교수가 두 명 있다고 한다.
+-- 그 교수의 이름과 주민번호를 조회하는 SQL을 작성하시오.
 
 SELECT PROFESSOR_NAME, PROFESSOR_SSN
 FROM TB_PROFESSOR
 WHERE LENGTH(PROFESSOR_NAME) > 3 OR
 LENGTH(PROFESSOR_NAME) < 3;
 
-/** 3번 **/
 
+-------------------------------------------------------------------
+/** 3번 **/
+-- 춘 기술대학교의 남자 교수들의 이름과 나이를 나이 오름차순으로 조회하시오.
+-- (단, 교수 중 2000년 이후 출생자는 없으며 출력 헤더는 "교수이름"으로 한다.
+-- 나이는 '만'으로 계산한다.)
 SELECT 
 	PROFESSOR_NAME AS 교수이름,
 	FLOOR(MONTHS_BETWEEN(SYSDATE, TO_DATE('19'||SUBSTR(PROFESSOR_SSN,0,6), 'YYYYMMDD'))/12) AS 나이
@@ -31,14 +41,21 @@ FLOOR(MONTHS_BETWEEN(
 	) / 12) AS "만 나이"
 **/
 
+
+-------------------------------------------------------------------
 /** 4번 **/
+-- 교수들의 이름 중 성을 제외한 이름만 조회하시오. 출력 헤더는 "이름"이 찍히도록 한다.
+-- (성이 2자인 경우의 교수는 없다고 가정)
 
 SELECT 
 	SUBSTR( PROFESSOR_NAME, 2 ) as 이름
 FROM TB_PROFESSOR 
 
-
+-------------------------------------------------------------------
 /** 5번 **/
+-- 춘 기술대학교의 재수생 입학자를 조회하시오.
+-- (19살에 입학하면 재수를 하지 않은 것!)
+
 SELECT 
 	STUDENT_NO, 
 	STUDENT_NAME
@@ -53,8 +70,10 @@ WHERE
 	) AS "한국식 나이",
  * */	
 
-
+-------------------------------------------------------------------
 /** 6번 **/
+-- 춘 기술대학교의 2000년도 이후 입학자들은 학번이 A로 시작하게 되어있다.
+-- 2000년도 이전 학번을 받은 학생들의 학번과 이름 조회하는 SQL을 작성하시오.
 
 SELECT 
 	STUDENT_NO,
@@ -63,14 +82,22 @@ FROM TB_STUDENT
 WHERE STUDENT_NO NOT LIKE 'A%';
 
 
+-------------------------------------------------------------------
 /** 7번 **/
+-- 학번이 A517178인 한아름 학생의 학점 총 평점을 구하는 SQL문을 작성하시오.
+-- 단, 이때 출력 화면의 헤더는 "평점"이라고 찍히게 하고,
+-- 점수는 반올림하여 소수점 이하 한자리까지만 표시한다.
+
 SELECT 
 	ROUND(AVG(POINT),1)
 FROM TB_GRADE 
 WHERE STUDENT_NO ='A517178'
 GROUP BY STUDENT_NO;
 
+-------------------------------------------------------------------
 /** 8번 **/
+-- 학과별 학생 수를 구하여 "학과번호", "학생수(명)"의 형태로 조회하시오.
+
 SELECT 
 	DEPARTMENT_NO AS 학과번호,
 	COUNT(DEPARTMENT_NO) AS "학생수(명)"
@@ -79,14 +106,23 @@ GROUP BY DEPARTMENT_NO
 ORDER BY DEPARTMENT_NO ASC;
 
 
+-------------------------------------------------------------------
 /** 9번 **/
+-- 지도 교수를 배정받지 못한 학생의 수를 조회하시오.
+
 SELECT 
 	COUNT(*)
 FROM TB_STUDENT 
 GROUP BY COACH_PROFESSOR_NO
 HAVING COACH_PROFESSOR_NO IS NULL;
 
+
+-------------------------------------------------------------------
 /** 10번 **/
+-- 학번이 A112113인 김고운 학생의 년도 별 평점을 구하는 SQL문을 작성하시오.
+-- 단, 이때 출력화면의 헤더는 "년도", "년도 별 평점"이라고 찍히게 하고,
+-- 점수는 반올림하여 소수점 이하 한자리까지만 표시한다
+
 SELECT 
 	SUBSTR(TERM_NO,0,4) AS 년도,
 	ROUND(AVG(POINT),1) AS "년도 별 평점"
@@ -95,19 +131,25 @@ WHERE STUDENT_NO ='A112113'
 GROUP BY SUBSTR(TERM_NO,0,4)
 ORDER BY 년도 ASC;
 
+
+-------------------------------------------------------------------
 /** 11번 **/
+-- 학과 별 휴학생 수를 파악하고자 한다.
+-- 학과 번호와 휴학생 수를 조회하는 SQL을 작성하시오.
+
 SELECT 
 	DEPARTMENT_NO AS 학과코드명,
 	SUM(DECODE(
 		ABSENCE_YN,
-		'Y', TO_NUMBER(1),
-		'N', TO_NUMBER(0) 
-	)) AS "휴학생 수"
+		'Y', 1,
+		'N', 0 
+	)) AS "휴학생 수", -- 복잡하게 생각한것
+	COUNT(DECODE(ABSENCE_YN, 'Y','휴학생')) AS "휴학생 수" -- COUNT 이용하기
 FROM TB_STUDENT 
 GROUP BY DEPARTMENT_NO
 ORDER BY DEPARTMENT_NO ASC;
 
-
+-------------------------------------------------------------------
 -- GPT의 복잡한 답을 보았다.
 SELECT 
 	D.DEPARTMENT_NO AS "학과코드명",
@@ -121,8 +163,10 @@ ORDER BY
 	D.DEPARTMENT_NO ASC;
 
 
-
+-------------------------------------------------------------------
 /**12번**/
+-- 춘 대학교에 다니는 동명이인인 학생들의 이름, 동명인 수를 조회하시오.
+
 SELECT 
 	STUDENT_NAME ,
 	COUNT(*) 
@@ -131,12 +175,14 @@ GROUP BY STUDENT_NAME
 HAVING COUNT(STUDENT_NAME) > 1
 ORDER BY STUDENT_NAME ASC;
 
-
+-------------------------------------------------------------------
 /** 13 **/
-
+-- 학번이 A112113인 김고운 학생의 학점을 조회하려고 한다.
+-- 년도, 학기 별 평점과 년도 별 누적 평점, 총 평점을 구하는 SQL을 작성하시오.
+-- (단, 평점은 소수점 1자리까지만 반올림하여 표시한다.)
 SELECT 
-	SUBSTR(TERM_NO,0,4) AS 년도,
- 	SUBSTR(TERM_NO,5,2) AS 학기,
+	NVL(SUBSTR(TERM_NO,0,4),' ') AS 년도,
+ 	NVL(SUBSTR(TERM_NO,5,2),' ') AS 학기,
 	ROUND(AVG(POINT),1) AS 평점
 FROM TB_GRADE  
 WHERE STUDENT_NO = 'A112113'
